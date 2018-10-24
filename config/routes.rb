6d9b4dad175e7a3f10ef8bdced07460c2c33465d
Marianna Espindola <bymarimari@hotmail.com>
2018-10-24 21:05:26 +0000

    Correções

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :filmes
+   get 'principal/index'
+   
+   resources :filmes
+ 
   get 'filmes/index'
-root "application#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
