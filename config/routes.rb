9bd4dc59745e17b973212092ce83563c2eece22c
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-10-24 21:14:35 +0000

    arrumando rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :filmes
   get 'filmes/index'
+  get 'principal/index'
 root "application#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
