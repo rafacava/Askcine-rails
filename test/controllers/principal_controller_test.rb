6753ca9cc39eaae56dc6dd92bf5fbf4f0d07a295
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-10-30 12:34:34 +0000

    arrumando o teste do controller principal (ver oq deu de errado)

--- test/controllers/principal_controller_test.rb
+++ test/controllers/principal_controller_test.rb
@@ -5,7 +5,8 @@ class PrincipalControllerTest < ActionDispatch::IntegrationTest
     get principal_index_url
     assert_response :success
     assert_select "title", "Askcine"
-    assert_select "#modal1.h4", "Login"
+    assert_select "h2", "Sobre o Projeto"
+    assert_select "#h3dologin", "Você ainda não efetuou o login"
     assert_select "h1", "Bem vindo ao AskCine"
     assert_response :success
   end
