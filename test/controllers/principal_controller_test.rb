f54baf22ba9a3d3c139f2aa2a3ee3f32cd07825d
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-10-30 03:23:55 +0000

    terminando teste da index e removendo duplicidade encontrada em filmes/index

--- test/controllers/principal_controller_test.rb
+++ test/controllers/principal_controller_test.rb
@@ -4,7 +4,8 @@ class PrincipalControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get principal_index_url
     assert_response :success
-    assert_select "title", "AskCine"
+    assert_select "title", "Askcine"
+    assert_select "#modal1.h4", "Login"
     assert_select "h1", "Bem vindo ao AskCine"
     assert_response :success
   end
