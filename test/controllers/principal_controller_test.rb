9e66aae3cf0822397dcfb7d4d0480f530ad4f411
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-10-30 02:46:31 +0000

    arrumando div -quebrada- no modal de login e criando o teste da pagina principal

--- test/controllers/principal_controller_test.rb
+++ test/controllers/principal_controller_test.rb
@@ -4,6 +4,8 @@ class PrincipalControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get principal_index_url
     assert_response :success
+    assert_select "title", "AskCine"
+    assert_select "h1", "Bem vindo ao AskCine"
+    assert_response :success
   end
-
 end
