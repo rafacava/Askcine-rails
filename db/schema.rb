9a0fa5e3689edcfe60f0b58e31f8854b8cb6c451
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-11-18 07:42:13 +0000

    saudacao pronta

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_061953) do
+ActiveRecord::Schema.define(version: 2018_11_18_073001) do
 
   create_table "filmes", force: :cascade do |t|
     t.integer "cd_filme"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_11_18_061953) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
