27a8baa3507cb891a6685ea6473da23f5ca6d808
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-11-28 07:17:55 +0000

    colocando destaques na home

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -1,5 +1,6 @@
 class PrincipalController < ApplicationController
   def index
     @filmes = Filme.all.limit(4).order('created_at DESC')
+    @destaques = Destaque.all.limit(4).order('created_at DESC')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_062607) do
+ActiveRecord::Schema.define(version: 2018_11_28_063738) do
 
   create_table "cidades", force: :cascade do |t|
     t.string "nome"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2018_11_28_062607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cidade_id"], name: "index_cinemas_on_cidade_id"
+  end
+
+  create_table "destaques", force: :cascade do |t|
+    t.string "titulo"
+    t.string "imagem"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "filmes", force: :cascade do |t|
