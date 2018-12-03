a2d6232f5d3ca35bff3ce037e59dd98bd8563f76
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2018-12-03 14:38:38 +0000

    adequando o novo controller ao projeto

--- app/controllers/buscar_controller.rb
+++ app/controllers/buscar_controller.rb
@@ -1,8 +1,21 @@
 class BuscarController < ApplicationController
   def cinemas
+    
     @cinemas = Cinema.all
+    if params[:search]
+      @cinemas = Cinema.search(params[:search])
+    else
+      @cinemas = Cinema.all
+    end
   end
+    
 
   def filme
+    @filmes = Filme.all
+    if params[:search]
+      @filmes = Filme.search(params[:search])
+    else
+      @filmes = Filme.all
+    end
   end
 end
