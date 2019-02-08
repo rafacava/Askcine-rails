2ba22ee4eafd02b4ea2b624da6e5f7bf73da887a
Rafael Cavalcante <rafaeldarkvaider@gmail.com>
2019-02-08 20:10:15 +0000

    mudando o limite de filmes na pagina principal de 4 para 3

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -1,6 +1,6 @@
 class PrincipalController < ApplicationController
   def index
-    @filmes = Filme.all.limit(4).order('created_at DESC')
+    @filmes = Filme.all.limit(3).order('created_at DESC')
     @destaques = Destaque.all.limit(4).order('created_at DESC')
     @postagems = Postagem.all.limit(4).order('created_at DESC')
   end
