d12154245e5dae7f27a39788e272dc2ce9abae8c
rafacava <rafaeldarkvaider@gmail.com>
2019-07-29 11:39:08 +0000

    atualizando gem para rodar no codenvy

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_021352) do
-
-  create_table "categoria", force: :cascade do |t|
-    t.string "nome"
-    t.string "descricao"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_04_15_223902) do
 
   create_table "cidades", force: :cascade do |t|
     t.string "nome"
