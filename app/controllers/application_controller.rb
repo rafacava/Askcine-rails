fc9d5c4c0bb6d04def7775c60dba3cdafccde574
Ubuntu <ubuntu@ip-172-31-19-18.us-east-2.compute.internal>
2019-09-19 16:07:47 +0000

    adcionando imagens ao usuario (avatar)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
-    def index
-    end
+     protect_from_forgery with: :exception
+
+     before_action :configure_permitted_parameters, if: :devise_controller?
+
+     protected
+
+          def configure_permitted_parameters
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar, :email, :password)}
+
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :current_password)}
+          end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   def username
     return self.email.split('@')[0].capitalize
   end
+  
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://linguaviva.com/wp-content/uploads/2017/03/facebook-avatar.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
